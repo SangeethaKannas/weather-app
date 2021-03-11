d7a9e7473b9c8d2cf5d9c032136bbb3b65ba1802
Sangeetha Kanna <sangeetha.kanna@hcl.com>
2021-03-11 08:44:30 +0530

    Modifying the results as per new design

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,63 +1,72 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-const Result = ({obj}) => {
-
+const Result = ({ obj }) => {
   const main = obj ? obj.main : {};
+  const weather = obj && obj.weather && obj.weather[0] ? obj.weather[0] : {};
   let minTemp = 0;
   let maxTemp = 0;
-  if(main) {
-    const temp = parseInt(obj.main.temp)
+  if (main) {
+    const temp = parseInt(main.temp);
     var cel = temp - 273.15;
     cel = cel.toFixed(2);
 
     //conversion of kelvin temperatures to centigrade
-    maxTemp = (obj.main.temp_min - 273.15).toFixed(2);
-    minTemp = (obj.main.temp_max - 273.15).toFixed(2);
+    maxTemp = (main.temp_min - 273.15).toFixed(2);
+    minTemp = (main.temp_max - 273.15).toFixed(2);
   }
-  console.log(obj)
+  console.log(obj);
 
   return (
-      <section>
-        {typeof main !== "undefined"  ?
-        <div className="card">
+    <section>
+      {typeof main !== "undefined" ? (
+        <div>
           <h2>{obj.name}</h2>
-          <h3>{obj.weather[0].description}
-            <span>Wind {obj.wind.speed} km/h <span className="dot">•</span> humidity {obj.main.humidity}%</span>
+          <div>
+            <img
+              src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`}
+              alt={weather.description}
+              width="120"
+            />
+            <h3>
+              {weather.description}
             </h3>
-          <h2>{cel} &#8451;</h2>
-          <div class="sky">
-              <div class="sun"></div>
-              <div class="cloud">
-                  <div class="circle-small"></div>
-                  <div class="circle-tall"></div>
-                  <div class="circle-medium"></div>
-              </div>
+            
           </div>
           <table>
-              <tr>
-                  <td>Pressure</td>
-                  <td>Cloudiness</td>
-                  <td>Max.temp</td>
-                  <td>Min temp</td>
-              </tr>
-              <tr>
-                  <td>{obj.main.pressure} hpa</td>
-                  <td>{obj.clouds.all}%</td>
-                  <td>{maxTemp} &#8451;</td>
-                  <td>{minTemp} &#8451;</td>
-              </tr>
+            <tr>
+              <td>Pressure</td>
+              <td>Cloudiness</td>
+              <td>Wind</td>
+              <td>Humidity</td>
+            </tr>
+            <tr>
+              <td>{main.pressure} hpa</td>
+              <td>{obj.clouds.all} %</td>
+              <td>{obj.wind.speed} km/h</td>
+              <td>{main.humidity} %</td>
+            </tr>
+            <tr>
+              <td>Temperature</td>
+              <td>Max.temp</td>
+              <td>Min temp</td>
+            </tr>
+            <tr>
+              <td>{cel} &#8451;</td>
+              <td>{maxTemp} &#8451;</td>
+              <td>{minTemp} &#8451;</td>
+            </tr>
           </table>
         </div>
-        :
-         <div></div>
-        }
-      </section>
-      );
-}
+      ) : (
+        <div></div>
+      )}
+    </section>
+  );
+};
 
 export default Result;
 
 Result.propTypes = {
-  obj: PropTypes.object.isRequired
-}
+  obj: PropTypes.object.isRequired,
+};
